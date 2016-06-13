9efcb65e11af99f532eb5ec53ccd7e73a32629aa
spacime <spacimeulim@gmail.com>
2016-06-13 14:39:39 -0700

    dockstore search ui build based on yeoman

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,32 @@
  * # MainCtrl
  * Controller of the dockstoreSearchUiApp
  */
+
 angular.module('dockstoreSearchUiApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $http) {
+        $scope.info = "";
+        $scope.search_query = {
+            text: ""
+        };
+        var len_pre = 0
+
+        $scope.change = function () {
+
+            if($scope.search_query.text.length > 2 && $scope.search_query.text.length > len_pre) {
+              $scope.info = $scope.search_query.text;
+
+              $http({
+                  method: "GET",
+                  url: "http://127.0.0.1:5000/search_keywords=" + $scope.info,
+                  data: {search_keywords : 'bri'}
+              }).then(function mySucces(response) {
+                  // $scope.myWelcome = JSON.parse(response.data);
+                  console.log(response);
+                  $scope.tools = response.data;
+              }, function myError(response) {
+                  $scope.tools = response.statusText;
+              });
+            }
+            len_pre = $scope.search_query.text.length;
+        };
+    });
